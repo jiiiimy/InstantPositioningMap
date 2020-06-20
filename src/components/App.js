cb58846279a91f0d2b1a19047938005a5d5399b7
Ryota Yajima <ryotayajima@gmail.com>
2020-06-20 10:18:08 +0900

    add react-helmet and meta info

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Helmet} from 'react-helmet';
 import '../assets/css/App.css';
 import SideArea from './side';
 import MainArea from './main';
@@ -7,6 +8,26 @@ class App extends Component {
   render() {
     return (
       <div className="container _all">
+        <Helmet>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+          <meta property="og:title" content="InstantQuadrant" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://instantquadrant.netlify.app" />
+          <meta property="og:image" content="https://instantquadrant.netlify.com/thumb.jpeg" />
+          <meta property="og:site_name" content="InstantQuadrant" />
+          <meta property="og:description" content="InstantQuadrantは、ポジショニングマップや四象限の図をを手軽に作成するWebアプリです。表をさくっと作ってブログや資料作成の時間を短縮しましょう。" />
+          <meta property="fb:app_id" content="2471674503145611" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@jiiiimy00" />
+          <meta name="twitter:player" content="@jiiiimy00" />
+          <link rel="shortcut icon" href="./favicon.ico" />
+          <link rel="apple-touch-icon" href="./apple-icon.png" />
+          <link rel="icon" type="image/png" href="./android-icon-256x256.png" />
+          <title>InstantQuadrant</title>
+          <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:400,700&display=swap" rel="stylesheet" />
+        </Helmet>
         <SideArea />
         <MainArea />
       </div>
